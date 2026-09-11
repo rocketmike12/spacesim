0fa3f1d9054ef62e9cfe44f0971522cdbe46d7ec
rocketmike12 <mg.korop@gmail.com>
2026-09-11 22:26:42 +0300

    command line args parsing; json format

--- spacesim.cpp
+++ spacesim.cpp
@@ -2,18 +2,21 @@
 // Copyright (c) 2026 rocketmike12
 // Licensed under the MIT License (see LICENSE)
 
-#include <algorithm>
 #include <bits/stdc++.h>
+#include <cstring>
+#include <filesystem>
+#include <nlohmann/detail/macro_scope.hpp>
 #include <raylib.h>
+#include <nlohmann/json.hpp>
 
+using json = nlohmann::json;
 using namespace std;
-
-const int WIDTH = 1024;
-const int HEIGHT = 1024;
 
 struct Vector2D {
 	float x;
 	float y;
+
+	NLOHMANN_DEFINE_TYPE_INTRUSIVE(Vector2D, x, y);
 };
 
 struct Body {
@@ -22,89 +25,169 @@ struct Body {
 	float m;
 	float R;
 	Vector2D v;
+
+	NLOHMANN_DEFINE_TYPE_INTRUSIVE(Body, x, y, m, R, v);
 };
 
-const float G = 100.0f;
-const float EPSILON = 700.0f;
-const float dT = 0.01667f;
+struct System {
+	int width;
+	int height;
+	int fps = 0;
+	float G;
+	float epsilon;
+	vector<Body> bodies;
+
+	NLOHMANN_DEFINE_TYPE_INTRUSIVE(System, width, height, fps, G, epsilon, bodies);
+};
+
+bool validate_json(const json& j) {
+    const vector<string> required_fields = {
+        "width", "height", "fps", "G", "epsilon", "bodies"
+    };
+    
+    for (const auto& field : required_fields) {
+        if (!j.contains(field)) {
+            cout << "ERROR: field missing: " << field << '\n';
+            return false;
+        }
+    }
+
+    if (!j["bodies"].is_array()) {
+        cout << "ERROR: field 'bodies' must be an array\n";
+        return false;
+    }
+
+    for (const auto& b : j.at("bodies")) {
+        const vector<string> body_fields = {"x", "y", "m", "R", "v"};
+        for (const auto& field : body_fields) {
+            if (!b.contains(field)) {
+                cout << "ERROR: field missing: bodies[i]." << field << '\n';
+                return false;
+            }
+        }
+
+        if (!b.at("v").contains("x")) {
+            std::cout << "field missing: bodies[i].v.x\n";
+            return false;
+        }
+
+        if (!b.at("v").contains("y")) {
+            std::cout << "field missing: bodies[i].v.y\n";
+            return false;
+        }
+    }
+
+    return true;
+}
+
+int parse_system (const json& j, System& s) {
+	if (!validate_json(j)) return -1;
+
+	j.at("width").get_to(s.width);
+	j.at("height").get_to(s.height);
+	j.at("fps").get_to(s.fps);
+	j.at("G").get_to(s.G);
+	j.at("epsilon").get_to(s.epsilon);
+	j.at("bodies").get_to(s.bodies);
+
+	return 0;
+}
 
 int main (int argc, char* argv[]) {
-	bool warp = false;
+	bool _warp = false;
+	System _system;
 
 	if (argc > 0) {
-		for (int i = 0; i < argc; i++) {
-			warp = warp || !strcmp(argv[i], "--warp");
+		for (int i = 1; i < argc; i++) {
+			if (!strcmp(argv[i], "-w") || !strcmp(argv[i], "--warp")) {
+				_warp = true;
+				continue;
+			}
+
+			if (!_system.fps) {
+				if (!filesystem::exists(argv[i])) {
+					cout << "ERROR: " << string(argv[i]) << ": no such file\n";
+					return -1;
+				}
+
+				ifstream f(argv[i]);
+				if(parse_system(json::parse(f), _system) != 0) return -1;
+
+				continue;
+			}
+		}
+
+		if (!_system.fps) {
+			cout << "ERROR: no system file provided\n";
+			return -1;
 		}
 	}
-	
-    InitWindow(WIDTH, HEIGHT, "spacesim");
-    SetTargetFPS(60);
 
-	float halfWidth = (float)WIDTH / 2;
-	float halfHeight = (float)HEIGHT / 2;
+	const float G = _system.G;
+	const float EPSILON = _system.epsilon;
+	const float dT = 1.0f / _system.fps;
 
-	vector<Body> system = {
-		{ halfWidth - 64, halfHeight - 64, 10000, 2, { 20.0f, 20.0f }},
-		{ halfWidth + 64, halfHeight + 64, 10000, 2, { -20.0f, -20.0f }},
-		{ halfWidth + 104, halfHeight + 32, 10000, 2, { -20.0f, -4.0f }},
-	};
+	InitWindow(_system.width, _system.height, "spacesim");
+	SetTargetFPS(_system.fps);
 
-    while (!WindowShouldClose()) {
-		for (int i = 0; i < (int)system.size(); i++) {
+	vector<Body> bodies = _system.bodies;
+
+	while (!WindowShouldClose()) {
+		for (int i = 0; i < (int)bodies.size(); i++) {
 			float total_aX = 0.0f;
 			float total_aY = 0.0f;
 
-			for (int j = 0; j < (int)system.size(); j++) {
+			for (int j = 0; j < (int)bodies.size(); j++) {
 				if (i == j) continue;
 
-				float dX = system[j].x - system[i].x;
-				float dY = system[j].y - system[i].y;
+				float dX = bodies[j].x - bodies[i].x;
+				float dY = bodies[j].y - bodies[i].y;
 				float r = sqrt((dX*dX) + (dY*dY));
 
-				float F = G * ((system[i].m * system[j].m) / ((r*r) + EPSILON));
+				float F = G * ((bodies[i].m * bodies[j].m) / ((r*r) + EPSILON));
 
-				total_aX += (F * dX) / (system[i].m * r);
-				total_aY += (F * dY) / (system[i].m * r);
+				total_aX += (F * dX) / (bodies[i].m * r);
+				total_aY += (F * dY) / (bodies[i].m * r);
 			}
 
 			// cout << "{ " << total_aX << " " << total_aY << " }" << "\n";
 
-			system[i].v.x += total_aX * dT;
-			system[i].v.y += total_aY * dT;
+			bodies[i].v.x += total_aX * dT;
+			bodies[i].v.y += total_aY * dT;
 		}
 
-		for (auto &body: system) {
+		for (auto &body: bodies) {
 			body.x += body.v.x * dT;
 			body.y += body.v.y * dT;
 
-			if (warp && body.x > WIDTH) {
-				body.x = body.x - WIDTH;
+			if (_warp && body.x > _system.width) {
+				body.x = body.x - _system.width;
 			}
 
-			if (warp && body.x < 0) {
-				body.x = body.x + WIDTH;
+			if (_warp && body.x < 0) {
+				body.x = body.x + _system.width;
 			}
 
-			if (warp && body.y > HEIGHT) {
-				body.y = body.y - HEIGHT;
+			if (_warp && body.y > _system.height) {
+				body.y = body.y - _system.height;
 			}
 
-			if (warp && body.y < 0) {
-				body.y = body.y + HEIGHT;
+			if (_warp && body.y < 0) {
+				body.y = body.y + _system.height;
 			}
 		}
 
-        BeginDrawing();
-        ClearBackground(BLACK);
+		BeginDrawing();
+		ClearBackground(BLACK);
 
-		for (const auto body : system) {
+		for (const auto body : bodies) {
 			DrawRectangle(body.x - body.R, body.y - body.R, body.R * 2, body.R * 2, WHITE);
 		}
 
-        EndDrawing();
+		EndDrawing();
 	}
 
-    CloseWindow();
+	CloseWindow();
 
 	return 0;
 }
